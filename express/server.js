fb89020faaffe4ff049b4d1b4c0f5c2fbbe6a5cb
wanghuilinqq <13679074511@163.com>
2019-04-12 21:34:13 +0800

    gt03

--- express/server.js
+++ express/server.js
@@ -7,7 +7,31 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write(`<head>
+  <meta charset="UTF-8"/>
+  <title>Blog</title>
+  <link rel="stylesheet" href="./public/stylesheets/style.css">
+  <meta http-equiv ="Content-Security-Policy" content="upgrade-insecure-requests">
+</head>
+<body>
+
+<header>
+  <h1>我的博客</h1>
+</header>
+
+<nav>
+  <span><a title="主页" href="/">发表博客</a></span>
+</nav>
+<article>
+  <form method="post" action="/">
+      标题：<br/>
+      <input type="text" name="title" id="title"/><br/>
+      正文：<br/>
+      <textarea name="text" rows="20" cols="100" id="text"></textarea><br/>
+      <input type="button" value="发表" id="btnPost" />
+  </form>
+</article>
+</body>`);
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
