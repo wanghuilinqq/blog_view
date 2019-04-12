aac4dc5c2677c14703f6413e40316a3647660566
wanghuilinqq <13679074511@163.com>
2019-04-12 21:57:16 +0800

    ds

--- express/server.js
+++ express/server.js
@@ -3,31 +3,12 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const router = express.Router();
+// app.use(express.static(path.join(__dirname,'/public')));
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`
-<header>
-  <h1>我的博客</h1>
-</header>
-
-<nav>
-  <span><a title="主页" href="/">发表博客</a></span>
-</nav>
-<article>
-  <form method="post" action="/">
-      标题：<br/>
-      <input type="text" name="title" id="title"/><br/>
-      正文：<br/>
-      <textarea name="text" rows="20" cols="100" id="text"></textarea><br/>
-      <input type="button" value="发表" id="btnPost" />
-  </form>
-</article>
-`);
-  res.end();
+    res.render('look.html')
 });
-
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
