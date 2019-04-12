aa83ab64555beb8917e63da9dca1ba8d38795aa7
wanghuilinqq <13679074511@163.com>
2019-04-12 23:06:05 +0800

    yytt

--- express/server.js
+++ express/server.js
@@ -7,32 +7,33 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const router = express.Router();
 router.post('/', (req, res) => {
+    console.log(req);
     let title = req.body.title ? req.body.title : 'null';
     let text = req.body.text ? req.body.text : 'null';
     let json = {
       "title":title,
       "text":text
     }
-    // fs.writeFile('../src/data.json',json,'utf-8',function(err,data){
-    //   if(err){
-    //     throw err;
-    //   }
-    // })
-    // res.writeHead(200, { 'Content-Type': 'text/html' });
-    // res.write(`
-    // <div>
-    // <span id="t3"></span>
-    // <span>
-    //     <h3> <strong style="color: red" id="t1"></strong>${JSON.stringify(req)}</h3>
-    // </span>
-    // <span>
-    //      <strong style="color: darkgreen" id="t2">${text}</strong>
-    // </span>
+    fs.writeFile('../src/data.json',json,'utf-8',function(err,data){
+      if(err){
+        throw err;
+      }
+    })
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`
+    <div>
+    <span id="t3"></span>
+    <span>
+        <h3> <strong style="color: red" id="t1"></strong>${JSON.stringify(req.body)}</h3>
+        <h3> <strong style="color: red" id="t1"></strong>${JSON.stringify(req)}</h3>
+    </span>
+    <span>
+         <strong style="color: darkgreen" id="t2">${text}</strong>
+    </span>
 
-    // </div>
-    // `);
-    // res.end();
-    res.end(JSON.stringify(req))
+    </div>
+    `);
+    res.end();
 });
 
 app.use(bodyParser.json());
