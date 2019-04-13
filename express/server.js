afb25370f4d61a165456c31f911e881994495a1e
wanghuilinqq <13679074511@163.com>
2019-04-13 19:22:35 +0800

    gdsfg

--- express/server.js
+++ express/server.js
@@ -10,14 +10,12 @@ let header = `<html>
             
             <body>
             
-              <header> <h1>我的博客</h1> </header>
-              
-              <nav>
-                  <span><a title="主页" href="https://condescending-franklin-acde9d.netlify.com/index.html">发表博客</a></span>
-              </nav>
-              <div id = "div_01">
+                <header> <h1>我的博客</h1> </header>
                 
-</div>
+                <nav>
+                    <span><a title="主页" href="https://condescending-franklin-acde9d.netlify.com/index.html">发表博客</a></span>
+                </nav>
+                <div id = "div_01"></div>
 
 `;
 
@@ -26,22 +24,38 @@ let flooter = `
     </html>
 `;
 module.exports.handler = (event, context, callback) => {
+
   let params = querystring.parse(event.body);
-  let html_2 = `
-      <script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
-       <script>
-        $(document).ready(function(){
-           var div =  document.getElementById("div_01");
-           var insertHtml = '';
-           insertHtml += '<div><span><h3> 标题-<strong style="color: red"></strong>'+${params.title}+'</h3></span>';
-           insertHtml += '<span>正文-<strong style="color: darkgreen" id="t2">'+${params.text}+'</strong></span> <br>';
-           innerHTML +=' <span>评论</span><hr><div><form>发表评论：<br><input type="text" name="comment" id="comment"><br> <input type="submit" value="评论" id="btnPost"></form></div>';
-           document.getElementById("insert").innerHTML = insertHtml;
-        });
-      </script> 
+  let html_02 = `
+                <script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
+                 <script>
+                  $(document).ready(function(){
+                   let temp_html ="<div>\\n" +
+                    "                    <span>\\n" +
+                    "                        <h3> 标题-<strong style=\\"color: red\\" id=\\"t1\\"></strong>${params.title}</h3>\\n" +
+                    "                    </span>\\n" +
+                    "                    <span>\\n" +
+                    "                         正文-<strong style=\\"color: darkgreen\\" id=\\"t2\\">${params.text}</strong>\\n" +
+                    "                    </span>\\n" +
+                    "                </div>\\n" +
+                    "                \\n" +
+                    "                <br>\\n" +
+                    "                <span>评论</span>\\n" +
+                    "                <hr>\\n" +
+                    "                <div>\\n" +
+                    "                    <form method=\\"post\\" action=\\"/.netlify/functions/look\\">\\n" +
+                    "                        发表评论：<br>\\n" +
+                    "                        <input type=\\"text\\" name=\\"comment\\" id=\\"comment\\"><br>\\n" +
+                    "                        <input type=\\"submit\\" value=\\"评论\\" id=\\"btnPost\\">\\n" +
+                    "                    </form>\\n" +
+                    "                </div>"
+                                    });
+                    $("#div_01").append(temp_html);
+                  </script>
+                
         `;
-  let html = header + html_2 + flooter;
-  callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 1001, body : html});
+  let html = header+ html_02 +flooter;
+  callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 200, body : html});
 
 };
 
