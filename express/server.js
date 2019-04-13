98249b213c1199a27db7cd295e0ba7bc04c3cea9
wanghuilinqq <13679074511@163.com>
2019-04-13 20:17:07 +0800

    gdsgds

--- express/server.js
+++ express/server.js
@@ -1,6 +1,29 @@
 import querystring from "querystring";
-import {header,html_02,header} from "server-local";
+let header =  `<html>
+            <head>
+                <meta charset="UTF-8"/>
+                <title>Blog</title>
+                <meta http-equiv ="Content-Security-Policy" content="upgrade-insecure-requests">
+                <link rel="stylesheet" href="https://condescending-franklin-acde9d.netlify.com/public/stylesheets/style.css">
+            </head>
+            
+            <body>
+            
+                <header> <h1>我的博客</h1> </header>
+                
+                <nav>
+                    <span><a title="主页" href="https://condescending-franklin-acde9d.netlify.com/index.html">发表博客</a></span>
+                </nav>
+                <div id = "div_01">
+             
+`;
+let html_02 = ``;
 
+let header = flooter = `
+    </div>
+    </body>
+    </html>
+`;
 module.exports.handler = (event, context, callback) => {
   let params = querystring.parse(event.body);
   let html =``;
--- server-local.js
+++ server-local.js
@@ -1,29 +1,3 @@
-
-const header =  `<html>
-            <head>
-                <meta charset="UTF-8"/>
-                <title>Blog</title>
-                <meta http-equiv ="Content-Security-Policy" content="upgrade-insecure-requests">
-                <link rel="stylesheet" href="https://condescending-franklin-acde9d.netlify.com/public/stylesheets/style.css">
-            </head>
-            
-            <body>
-            
-                <header> <h1>我的博客</h1> </header>
-                
-                <nav>
-                    <span><a title="主页" href="https://condescending-franklin-acde9d.netlify.com/index.html">发表博客</a></span>
-                </nav>
-                <div id = "div_01">
-             
-`;
-const html_02 = ``;
-const header = flooter = `
-    </div>
-    </body>
-    </html>
-`;
-
 const app = require('./express');
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
