9b79407b59d283d7af577405d45cd519296629dc
wanghuilinqq <13679074511@163.com>
2019-04-13 18:56:46 +0800

    gdsgrgre

--- express/look.js
+++ express/look.js
@@ -2,25 +2,6 @@ import querystring from "querystring";
 
 module.exports.handler = function(event, context, callback) {
   const params = querystring.parse(event.body);
-  fs.readFile('../src/data.json', 'utf8', function(err, data) {
-    if(err) {
-      callback({headers : {"content-type" : 'text/html'}, statusCode : 1001, body : String(err)});
-    }
-    let person = data.toString();
-    person = JSON.parse(person);
-    person.data.map(item => {
-      if(item.title == params.title) {
-        item.comment.push(params.comment);
-      }
-    });
-    let str = JSON.stringify(person);
-    fs.writeFile('../src/data.json', str, function(err, ret) {
-      if(err) {
-        callback({headers : {"content-type" : 'text/html'}, statusCode : 1001, body : String(err)});
-      }
-      callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 200, body : '评论成功'});
-    });
-  });
-
+  callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 200, body : '评论成功'});
 };
 
--- express/server.js
+++ express/server.js
@@ -1,71 +1,47 @@
 import querystring from "querystring";
-function foo(){
-  let p = new Promise((resolve,reject)=>{
-    
-  })
-}
-let fs = require('fs');
-module.exports.handler = (event, context,callback) => {
-  let params = querystring.parse(event.body);
-  new Promise()fs.readFile('../src/data.json', 'utf8', function(err, data) {
-    if(err) {
-      callback({headers : {"content-type" : 'text/html'}, statusCode : 1001, body : String(err)});
-    }
-    let person = data.toString();
-    person = JSON.parse(person);
-    let temp_param = {
-      "id" : person.data.length + 1,
-      "title" : params.title,
-      "text" : params.text,
-      "comment":[]
-    };
-    person.data.push(temp_param);
-    person.total = person.data.length;
-    let str = JSON.stringify(person);
-    fs.writeFile('../src/data.json', str,function(err,rest) {
-      if(err){
-        callback({headers : {"content-type" : 'text/html'}, statusCode : 1001, body : String(err)});
-      }
-      let html =  `<html>
+
+let header = `<html>
             <head>
                 <meta charset="UTF-8"/>
                 <title>Blog</title>
                 <meta http-equiv ="Content-Security-Policy" content="upgrade-insecure-requests">
                 <link rel="stylesheet" href="https://condescending-franklin-acde9d.netlify.com/public/stylesheets/style.css">
             </head>
+            
             <body>
             
-                <header> <h1>我的博客</h1> </header>
+              <header> <h1>我的博客</h1> </header>
+              
+              <nav>
+                  <span><a title="主页" href="https://condescending-franklin-acde9d.netlify.com/index.html">发表博客</a></span>
+              </nav>
+              <div id = "div_01">
                 
-                <nav>
-                    <span><a title="主页" href="https://condescending-franklin-acde9d.netlify.com/index.html">发表博客</a></span>
-                </nav>
-                
-                <div>
-                    <span id="t3"></span>
-                    <span>
-                        <h3> 标题-<strong style="color: red" id="t1"></strong>${params.title}</h3>
-                    </span>
-                    <span>
-                         正文-<strong style="color: darkgreen" id="t2">${params.text}</strong>
-                    </span>
-                </div>
-                
-                <br>
-                <br>
-                <span>评论</span>
-                <hr>
-                <div>
-                    <form method="post" action="/.netlify/functions/look">
-                        发表评论：<br>
-                        <input type="text" name="comment" id="comment"><br>
-                        <input type="submit" value="评论" id="btnPost">
-                    </form>
-                </div>
-            </body>
-        </html>`;
-      callback(null,{headers : {"content-type" : 'text/html'}, statusCode : 1001, body : html});
-    });
-  });
+</div>
+
+`;
+
+let flooter = `
+    </body>
+    </html>
+`;
+module.exports.handler = (event, context, callback) => {
+  let params = querystring.parse(event.body);
+  let html_2 = `
+      <script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
+       <script>
+        $(document).ready(function(){
+           var div =  document.getElementById("div_01");
+           var insertHtml = '';
+           insertHtml += '<div><span><h3> 标题-<strong style="color: red"></strong>'+${params.title}+'</h3></span>';
+           insertHtml += '<span>正文-<strong style="color: darkgreen" id="t2">'+${params.text}+'</strong></span> <br>';
+           innerHTML +=' <span>评论</span><hr><div><form>发表评论：<br><input type="text" name="comment" id="comment"><br> <input type="submit" value="评论" id="btnPost"></form></div>';
+           document.getElementById("insert").innerHTML = insertHtml;
+        });
+      </script> 
+        `;
+  let html = header + html_2 + flooter;
+  callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 1001, body : html});
+
 };
 
