ed820bb3bdc8c32392b3e9fc06d8cb2eaa753c1b
wanghuilinqq <13679074511@163.com>
2019-04-13 19:52:26 +0800

    gsger

--- express/server.js
+++ express/server.js
@@ -1,29 +1,4 @@
 import querystring from "querystring";
-let global_html = ``;
-let header = `<html>
-            <head>
-                <meta charset="UTF-8"/>
-                <title>Blog</title>
-                <meta http-equiv ="Content-Security-Policy" content="upgrade-insecure-requests">
-                <link rel="stylesheet" href="https://condescending-franklin-acde9d.netlify.com/public/stylesheets/style.css">
-            </head>
-            
-            <body>
-            
-                <header> <h1>我的博客</h1> </header>
-                
-                <nav>
-                    <span><a title="主页" href="https://condescending-franklin-acde9d.netlify.com/index.html">发表博客</a></span>
-                </nav>
-                <div id = "div_01"></div>
-                <script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
-
-`;
-
-let flooter = `
-    </body>
-    </html>
-`;
 module.exports.handler = (event, context, callback) => {
   let params = querystring.parse(event.body);
   if(params.title){
@@ -52,16 +27,12 @@ module.exports.handler = (event, context, callback) => {
                         "                </div>";
                         $("#div_01").append(temp_html);
                                         });
-                    
                   </script>
                 
         `;
-    global_html = header+ html_02 +flooter;
-    callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 200, body : global_html});
+    global.html = global.header+ html_02 +global.flooter;
   }else if(params.comment){
     let html_02 = `
-
-                <script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
                  <script>
                   $(document).ready(function(){
                        let temp_html ="<span><strong>${params.comment}</strong></span><br>";
@@ -71,11 +42,10 @@ module.exports.handler = (event, context, callback) => {
                   </script>
                 
         `;
-    global_html = header+ html_02 +flooter;
-    callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 200, body : global_html});
+    global.html = global.header+ html_02 +global.flooter;
   }
-
-
+  let global_html = global.html;
+  callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 200, body : global_html});
 
 };
 
--- server-local.js
+++ server-local.js
@@ -1,4 +1,27 @@
 
 const app = require('./express');
+global.header =  `<html>
+            <head>
+                <meta charset="UTF-8"/>
+                <title>Blog</title>
+                <meta http-equiv ="Content-Security-Policy" content="upgrade-insecure-requests">
+                <link rel="stylesheet" href="https://condescending-franklin-acde9d.netlify.com/public/stylesheets/style.css">
+            </head>
+            
+            <body>
+            
+                <header> <h1>我的博客</h1> </header>
+                
+                <nav>
+                    <span><a title="主页" href="https://condescending-franklin-acde9d.netlify.com/index.html">发表博客</a></span>
+                </nav>
+                <div id = "div_01"></div>
+                <script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
 
+`;
+global.header = flooter = `
+    </body>
+    </html>
+`;
+global.html = ``;
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
