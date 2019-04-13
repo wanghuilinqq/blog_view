4d33730ae82eea245b83c7c41978baaf66235466
wanghuilinqq <13679074511@163.com>
2019-04-13 20:54:10 +0800

    gsdgsf

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 import querystring from "querystring";
-let header =  `<html>
+
+let header = `<html>
             <head>
                 <meta charset="UTF-8"/>
                 <title>Blog</title>
@@ -24,38 +25,57 @@ let flooter = `
     </body>
     </html>
 `;
+let id = 1;
 module.exports.handler = (event, context, callback) => {
   let params = querystring.parse(event.body);
-  let html =``;
-  if(params.title){
+  let html = ``;
+  if(params.title) {
     html_02 += `
                        <div>
                               <span>
-                                  <h3> 标题-<strong style="color: red" id="t1"></strong>${params.title}</h3>
+                                  <h3> 标题-<strong style="color: red"></strong>${params.title}</h3>
                               </span>
                               <span>
-                                   正文-<strong style="color: darkgreen" id="t2">${params.text}</strong>
+                                   正文-<strong style="color: darkgreen">${params.text}</strong>
                               </span>
                           </div>
                         
                           <br>
                           <span>评论</span>
                           <hr>
-                          <div id="div_02"> </div>
+                          <div id="div_${id}"> </div>
                           <div>
                             <form method="post" action="/.netlify/functions/server">
                                 发表评论：<br>
+                                <input type="hidden" id="${id}"></input>
                                 <input type="text" name="comment" id="comment"><br>
                                <input type="submit" value="评论" id="btnPost">
                             </form>
                             </div>
                        </div>
         `;
-     html = header+ html_02 +flooter;
-  }else if(params.comment){
-    let html_02 = `<span><strong>${params.comment}</strong></span><br>`;
-     html = header+ html_02 +flooter;
+    id += 1;
+    html = header + html_02 + flooter;
+
+
+  } else if(params.comment) {
+    let query_str = "div_"+params.id;
+    let query_length = query_str.length;
+    let index_str = html_02.indexOf(query_str);
+    let html_03 = `<span><strong>${params.comment}</strong></span><br>`;
+    // let html_03_length = html_03.length;
+    html_02 = html_02.slice(0,index_str+query_length)+html_03+html_02.slice(index_str+query_length);
+
+    html = header + html_02+ flooter;
+
+
+    
+  } else if(params.myBlog) {
+    html = header + html_02 + flooter;
   }
+
+
+
   callback(null, {headers : {"content-type" : 'text/html'}, statusCode : 200, body : html});
 
 };
